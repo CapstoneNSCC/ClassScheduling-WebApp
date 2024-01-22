672c5c3be0e1f7210fac880555fb19631dbc1673
Kayla Gillam-Wright <kd.gillamwright@gmail.com>
2024-01-22 12:52:20 -0400

    Login-Screen

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using ClassScheduling_WebApp.Models;
+using Microsoft.AspNetCore.Http;
+
+namespace userAuthentication.Controllers
+{
+
+  public class LoginController : Controller
+  {
+
+    public IActionResult Index()
+    {
+      return View();
+    }
+
+    public IActionResult Submit(string myUsername, string myPassword)
+    {
+      WebLogin webLogin = new WebLogin(Connection.CONNECTION_STRING, HttpContext);
+
+      //update properties
+      webLogin.username = myUsername;
+      webLogin.password = myPassword;
+
+      //attempt unlock
+      if (webLogin.unlock())
+      {
+        return RedirectToAction("Index", "Admin");
+      }
+      else
+      {
+        //incorrect login - update feedback
+        ViewData["feedback"] = "Incorrect Login. Please try again...";
+      }
+
+      return View("Index");
+    }
+  }
+}
---
+++ Models/ScheduleManager.cs
@@ -0,0 +1,16 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace ClassScheduling_WebApp.Models
+{
+
+  public class ScheduleManager : DbContext
+  {
+
+    // ----------------------------------- gets and set
+
+  }
+}
