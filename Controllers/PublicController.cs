60607eeb97317424b70637f87e5886840cdbcd47
Kayla Gillam-Wright <kd.gillamwright@gmail.com>
2024-01-22 12:52:20 -0400

    Login-Screen

---
+++ Controllers/PublicController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ClassScheduling_WebApp.Models;
+
+namespace ClassScheduling_WebApp.Controllers
+{
+
+  public class PublicController : Controller
+  {
+    private ScheduleManager scheduleManager;
+
+    public PublicController(ScheduleManager myManager)
+    {
+      scheduleManager = myManager;
+    }
+
+    public IActionResult Index()
+    {
+      return View(scheduleManager);
+    }
+
+  }
+}
---
+++ Models/Connection.cs
@@ -0,0 +1,4 @@
+public class Connection
+{
+  public const string CONNECTION_STRING = "Server=localhost;Database=ClassSchedual;Uid=user;Pwd=secret;port=3306;SslMode=none;";
+}
