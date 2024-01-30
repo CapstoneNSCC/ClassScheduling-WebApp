b8007f64e1d8c49e1ccdba6595f73782b752a4e8
DMac96 <maccormackdylan@gmail.com>
2024-01-30 10:24:48 -0400

    edit timer to 20 minites for timer

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,7 +2,7 @@
 // for details on configuring this project to bundle and minify static web assets.
 function startAutoLogout() {
     // setup timeout that will occur when session has expired (20 minutes = 60000 * 20 milliseconds)
-    window.setTimeout(() => document.location = "/Login", 4500);
+    window.setTimeout(() => document.location = "/Login", 450000);
     // setup timeout that will occur when session about to expire to warn user (18 minutes = 60000 * 18 milliseconds)
     window.setTimeout(() => document.getElementById("lblExpire").innerHTML = "WARNING : Session is about to expire!", 300000);
     context.Session.SetString("auth", "false");
