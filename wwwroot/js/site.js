713f5f8175e187f8de2ab5a1f968a4d96562b6b8
DMac96 <maccormackdylan@gmail.com>
2024-01-31 08:48:32 -0400

    Logout timer readded

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -10,19 +10,28 @@ let passwordLength = 0;
 let loginBtn = document.getElementById("loginBtn");
 
 function onLoginUsernameChange(e) {
-  usernameLength = e;
-  checkLoginDisabled();
+    usernameLength = e;
+    checkLoginDisabled();
 }
 
 function onLoginPasswordChange(e) {
-  passwordLength = e;
-  checkLoginDisabled();
+    passwordLength = e;
+    checkLoginDisabled();
 }
 
 function checkLoginDisabled() {
-  if (usernameLength > 0 && passwordLength > 0) {
-    loginBtn.disabled = false;
-  } else {
-    loginBtn.disabled = true;
-  }
+    if (usernameLength > 0 && passwordLength > 0) {
+        loginBtn.disabled = false;
+    } else {
+        loginBtn.disabled = true;
+    }
+}
+
+function startAutoLogout() {
+    // setup timeout that will occur when session has expired (20 minutes = 60000 * 20 milliseconds)
+    window.setTimeout(() => document.location = "/Login", 450000);
+    // setup timeout that will occur when session about to expire to warn user (18 minutes = 60000 * 18 milliseconds)
+    window.setTimeout(() => document.getElementById("lblExpire").innerHTML = "WARNING : Session is about to expire!", 300000);
+    context.Session.SetString("auth", "false");
+    context.Session.SetString("user", _username);
 }
