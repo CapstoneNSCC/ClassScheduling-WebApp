c0d4b88247b94a47f166ea29baee2656232f4cb2
SantosPablo <pabloasantos@gmail.com>
2024-02-06 13:23:47 -0400

    Update database and connections

--- Models/Connection.cs
+++ Models/Connection.cs
@@ -1,4 +1,10 @@
 public class Connection
 {
-  public const string CONNECTION_STRING = "Server=localhost;Database=ClassSchedual;Uid=user;Pwd=secret;port=3306;SslMode=none;";
+    private static string Server = Environment.GetEnvironmentVariable("DB_SERVER") ?? "localhost";
+    private static string Database = Environment.GetEnvironmentVariable("DB_DATABASE") ?? "dbClassSchedule";
+    private static string Uid = Environment.GetEnvironmentVariable("DB_UID") ?? "user";
+    private static string Pwd = Environment.GetEnvironmentVariable("DB_PWD") ?? "secret";
+    private static string Port = Environment.GetEnvironmentVariable("DB_PORT") ?? "3306";
+
+    public static string CONNECTION_STRING => $"Server={Server};Database={Database};Uid={Uid};Pwd={Pwd};port={Port};SslMode=Required;";
 }
