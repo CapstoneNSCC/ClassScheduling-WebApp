cf1500cb089d56737ce91fba606c10d717c9780d
DMac96 <maccormackdylan@gmail.com>
2024-02-12 20:10:22 -0400

    added deploment information to connection model and ssl cert

--- Models/Connection.cs
+++ Models/Connection.cs
@@ -1,10 +1,12 @@
 public class Connection
 {
-    private static string Server = Environment.GetEnvironmentVariable("DB_SERVER") ?? "localhost";
+    private static string Server = Environment.GetEnvironmentVariable("DB_SERVER") ?? "localhost" ?? "capstoneteam6db.mysql.database.azure.com";
     private static string Database = Environment.GetEnvironmentVariable("DB_DATABASE") ?? "dbClassSchedule";
-    private static string Uid = Environment.GetEnvironmentVariable("DB_UID") ?? "user";
-    private static string Pwd = Environment.GetEnvironmentVariable("DB_PWD") ?? "secret";
+    private static string Uid = Environment.GetEnvironmentVariable("DB_UID") ?? "user" ?? "DylanMac";
+    private static string Pwd = Environment.GetEnvironmentVariable("DB_PWD") ?? "secret" ?? "Wer45Tgbvf";
     private static string Port = Environment.GetEnvironmentVariable("DB_PORT") ?? "3306";
 
-    public static string CONNECTION_STRING => $"Server={Server};Database={Database};Uid={Uid};Pwd={Pwd};port={Port};SslMode=Required;";
+    private static string sslPath = Environment.GetEnvironmentVariable("SSL_PATH") ?? "ssl/DigiCertGlobalRootCA.crt.pem";
+
+    public static string CONNECTION_STRING => $"Server={Server};Database={Database};Uid={Uid};Pwd={Pwd};port={Port};SslMode=Required;SslCa={sslPath};";
 }
