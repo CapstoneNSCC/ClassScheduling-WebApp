c033b46c43409e88c91b217f6084fce92310fcfb
DMac96 <maccormackdylan@gmail.com>
2024-02-12 20:20:44 -0400

    adjusting connection string

--- Models/Connection.cs
+++ Models/Connection.cs
@@ -1,9 +1,9 @@
 public class Connection
 {
-    private static string Server = Environment.GetEnvironmentVariable("DB_SERVER") ?? "localhost" ?? "capstoneteam6db.mysql.database.azure.com";
+    private static string Server = Environment.GetEnvironmentVariable("DB_SERVER") ?? "capstoneteam6db.mysql.database.azure.com";
     private static string Database = Environment.GetEnvironmentVariable("DB_DATABASE") ?? "dbClassSchedule";
-    private static string Uid = Environment.GetEnvironmentVariable("DB_UID") ?? "user" ?? "DylanMac";
-    private static string Pwd = Environment.GetEnvironmentVariable("DB_PWD") ?? "secret" ?? "Wer45Tgbvf";
+    private static string Uid = Environment.GetEnvironmentVariable("DB_UID") ?? "DylanMac";
+    private static string Pwd = Environment.GetEnvironmentVariable("DB_PWD") ?? "Wer45Tgbvf";
     private static string Port = Environment.GetEnvironmentVariable("DB_PORT") ?? "3306";
 
     private static string sslPath = Environment.GetEnvironmentVariable("SSL_PATH") ?? "ssl/DigiCertGlobalRootCA.crt.pem";
