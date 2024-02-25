52178a0966d4e5c9b8d6973659195e17a79923c4
Kayla Gillam-Wright <kd.gillamwright@gmail.com>
2024-02-25 00:39:50 -0400

    corrected spelling errors, adjusted return statement for Programs list to return programs in descending order for name, can now getProgramByID added AddProgram action to the admin controller started working on AddCourse action as well. Have the view for adding a program completed just need to add the functionality.

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using ClassScheduling_WebApp.Models;
+using System.Collections.Immutable;
 
 namespace ClassScheduling_WebApp.Controllers
 {
@@ -29,6 +30,49 @@ namespace ClassScheduling_WebApp.Controllers
       // Clear session data
       HttpContext.Session.Clear();
       return RedirectToAction("Index", "Login");
+    }
+
+
+    [Route("/Admin/AddProgram")]
+    public IActionResult AddProgram()
+    {
+      // if auth is not  = true, it re-directs to the login screen.
+      if (HttpContext.Session.GetString("auth") != "true")
+      {
+        return RedirectToAction("Index", "Login");
+      }
+
+      // construct course object that will be used to add a new course.
+      EducationalProgram program = new EducationalProgram
+      {
+        Name = "",
+      };
+      return View(scheduleManager);
+    }
+
+    [Route("/Admin/AddCourse/{programID:int}")]
+    public IActionResult AddCourse(int programID)
+    {
+      // if auth is not  = true, it re-directs to the login screen.
+      if (HttpContext.Session.GetString("auth") != "true")
+      {
+        return RedirectToAction("Index", "Login");
+      }
+
+      // construct course object that will be used to add a new course.
+      Courses course = new Courses
+      {
+        Code = "",
+        Name = "",
+        Professor = new User(),
+        EducationalProgram = scheduleManager.getProgramByID(programID)
+      };
+
+      //storing SelectList object in ViewBag
+      ViewBag.EducationalProgram = scheduleManager.getProgramByID(programID);
+      course.IdEducationalProgram = programID;
+      // pass it into the view for populating
+      return View(scheduleManager);
     }
   }
 }
