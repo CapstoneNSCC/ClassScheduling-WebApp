15388b4be2a16c5b1be7f168e32cc6779a1ae7ef
DMac96 <maccormackdylan@gmail.com>
2024-03-12 12:06:57 -0300

    programs edit and delete views added and working

--- Controllers/ProgramsController.cs
+++ Controllers/ProgramsController.cs
@@ -50,5 +50,70 @@ namespace ClassScheduling_WebApp.Controllers
             return RedirectToAction("Index", "Admin");
         }
 
+        [Route("/Programs/Update/{programID:int}")]
+        public IActionResult Update(int programID)
+        {
+            // if auth is not  = true, it re-directs to the login screen.
+            if (HttpContext.Session.GetString("auth") != "true")
+            {
+                return RedirectToAction("Index", "Login");
+            }
+
+            // find the program by the programID
+            ProgramModel program = _context.Programs.Find(programID);
+            //passing in program model to the view
+            return View("EditProgram", program);
+        }
+
+        public IActionResult UpdateSubmit(ProgramModel program)
+        {
+            // if auth is not  = true, it re-directs to the login screen.
+            if (HttpContext.Session.GetString("auth") != "true")
+            {
+                return RedirectToAction("Index", "Login");
+            }
+
+            // update the program in the list of programs
+            _context.Programs.Update(program);
+            //save changes to the database
+            _context.SaveChanges();
+            return RedirectToAction("Index", "Admin");
+        }
+
+        [Route("/Programs/Delete/{programID:int}")]
+        public IActionResult Delete(int programID)
+        {
+            // if auth is not  = true, it re-directs to the login screen.
+            if (HttpContext.Session.GetString("auth") != "true")
+            {
+                return RedirectToAction("Index", "Login");
+            }
+
+            // find the program by the programID
+            ProgramModel program = _context.Programs.Find(programID);
+
+            return View("DeleteProgram", program);
+        }
+
+        public IActionResult DeleteSubmit(ProgramModel program)
+        {
+            // if auth is not  = true, it re-directs to the login screen.
+            if (HttpContext.Session.GetString("auth") != "true")
+            {
+                return RedirectToAction("Index", "Login");
+            }
+            var existingProgram = _context.Programs.Find(program.Id);
+            if (existingProgram == null)
+            {
+                return RedirectToAction("Index", "Admin");
+            }
+
+            // remove the program from the list of programs
+            _context.Programs.Remove(existingProgram);
+            //save changes to the database
+            _context.SaveChanges();
+            return RedirectToAction("Index", "Admin");
+
+        }
     }
 }
