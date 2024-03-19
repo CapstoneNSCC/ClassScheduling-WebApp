65f6fe2946799e44a0e9cbf177d4c879249b68c9
SantosPablo <pabloasantos@gmail.com>
2024-03-19 10:00:36 -0300

    Fixing problem with two ProgramController (having erros yet)

--- Controllers/ProgramController.cs
+++ Controllers/ProgramController.cs
@@ -4,6 +4,7 @@ using ClassScheduling_WebApp.Models;
 using Microsoft.EntityFrameworkCore;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Collections.Immutable;
 
 namespace ClassScheduling_WebApp.Controllers
 {
@@ -16,7 +17,7 @@ namespace ClassScheduling_WebApp.Controllers
             _context = context;
         }
 
-        // Existing CRUD methods here...
+        
 
         // Fetches and displays details of a specific program by ID
         public async Task<IActionResult> GetProgramById(int programId)
@@ -54,6 +55,105 @@ namespace ClassScheduling_WebApp.Controllers
         private bool ProgramModelExists(int id)
         {
             return _context.Programs.Any(e => e.Id == id);
+        }
+
+        public IActionResult AddProgram()
+        {
+            // if auth is not  = true, it re-directs to the login screen.
+            if (HttpContext.Session.GetString("auth") != "true")
+            {
+                return RedirectToAction("Index", "Login");
+            }
+
+            // construct course object that will be used to add a new course.
+            ProgramModel program = new ProgramModel
+            {
+                Name = "",
+                Year = 1,
+            };
+            //passing in program model to the view
+            return View(program);
+        }
+
+        public IActionResult AddSubmit(ProgramModel program)
+        {
+            // if auth is not  = true, it re-directs to the login screen.
+            if (HttpContext.Session.GetString("auth") != "true")
+            {
+                return RedirectToAction("Index", "Login");
+            }
+
+            // add the program to the list of programs
+            _context.Programs.Add(program);
+            //save changes to the database
+            _context.SaveChanges();
+            return RedirectToAction("Index", "Admin");
+        }
+
+        [Route("/Programs/Update/{programID:int}")]
+        public IActionResult Update(int programID)
+        {
+            // if auth is not  = true, it re-directs to the login screen.
+            if (HttpContext.Session.GetString("auth") != "true")
+            {
+                return RedirectToAction("Index", "Login");
+            }
+
+            // find the program by the programID
+            ProgramModel program = _context.Programs.Find(programID);
+            //passing in program model to the view
+            return View("EditProgram", program);
+        }
+
+        public IActionResult UpdateSubmit(ProgramModel program)
+        {
+            // if auth is not  = true, it re-directs to the login screen.
+            if (HttpContext.Session.GetString("auth") != "true")
+            {
+                return RedirectToAction("Index", "Login");
+            }
+
+            // update the program in the list of programs
+            _context.Programs.Update(program);
+            //save changes to the database
+            _context.SaveChanges();
+            return RedirectToAction("Index", "Admin");
+        }
+
+        [Route("/Programs/Delete/{programID:int}")]
+        public IActionResult Delete(int programID)
+        {
+            // if auth is not  = true, it re-directs to the login screen.
+            if (HttpContext.Session.GetString("auth") != "true")
+            {
+                return RedirectToAction("Index", "Login");
+            }
+
+            // find the program by the programID
+            ProgramModel program = _context.Programs.Find(programID);
+
+            return View("DeleteProgram", program);
+        }
+
+        public IActionResult DeleteSubmit(ProgramModel program)
+        {
+            // if auth is not  = true, it re-directs to the login screen.
+            if (HttpContext.Session.GetString("auth") != "true")
+            {
+                return RedirectToAction("Index", "Login");
+            }
+            var existingProgram = _context.Programs.Find(program.Id);
+            if (existingProgram == null)
+            {
+                return RedirectToAction("Index", "Admin");
+            }
+
+            // remove the program from the list of programs
+            _context.Programs.Remove(existingProgram);
+            //save changes to the database
+            _context.SaveChanges();
+            return RedirectToAction("Index", "Admin");
+
         }
     }
 }
