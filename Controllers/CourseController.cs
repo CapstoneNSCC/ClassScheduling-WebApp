57b46228c483f79e39d637809e7cc28d00a9354a
Kayla Gillam-Wright <kd.gillamwright@gmail.com>
2024-03-30 00:11:59 -0300

    adjusted views for add course, changed flow for screens added the add professor in the add course fo convinence

--- Controllers/CourseController.cs
+++ Controllers/CourseController.cs
@@ -9,253 +9,253 @@ using Microsoft.AspNetCore.Http; // Required for Session
 
 namespace ClassScheduling_WebApp.Controllers
 {
-    public class CourseController : Controller
+  public class CourseController : Controller
+  {
+    private readonly ApplicationDbContext _context;
+
+    public CourseController(ApplicationDbContext context)
     {
-        private readonly ApplicationDbContext _context;
-
-        public CourseController(ApplicationDbContext context)
-        {
-            _context = context;
-        }
-
-        // Displays a list of all courses
-        public async Task<IActionResult> Index()
-        {
-            if (HttpContext.Session.GetString("auth") != "true")
-            {
-                return RedirectToAction("Index", "Login");
-            }
-
-            var courses = await _context.Courses.Include(c => c.Professor).Include(c => c.TechClasses).ToListAsync();
-            return View(courses);
-        }
-
-        // Displays details for a specific course
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (HttpContext.Session.GetString("auth") != "true")
-            {
-                return RedirectToAction("Index", "Login");
-            }
-
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var course = await _context.Courses
-                .Include(c => c.Professor)
-                .Include(c => c.TechClasses)
-                .FirstOrDefaultAsync(m => m.Id == id);
-
-            if (course == null)
-            {
-                return NotFound();
-            }
-
-            return View(course);
-        }
-
-        // // Shows the form to add a new course
-        // public IActionResult Add()
-        // {
-        //     if (HttpContext.Session.GetString("auth") != "true")
-        //     {
-        //         return RedirectToAction("Index", "Login");
-        //     }
-
-        //     PopulateProfessorsDropDownList();
-        //     PopulateProgramsDropDownList();
-        //     return View(new CourseModel()); // Passes an empty instance to the View
-        // }
-
-        
-        // Shows the form to add a new course for a specific program
-        [Route("/Course/AddCourse/{programID:int}")]
-        public IActionResult AddCourse(int programId)
-        {
-            if (HttpContext.Session.GetString("auth") != "true")
-            {
-                return RedirectToAction("Index", "Login");
-            }
-
-            PopulateProfessorsDropDownList();
-            PopulateProgramsDropDownList(programId); // Pass the programId to pre-select the program in the dropdown
-            
-            var courseModel = new CourseModel();
-            
-            courseModel.IdProgram = programId; // Pre-select the program if an ID was provided
-            
-            
-            return View("~/Views/Course/AddCourse.cshtml", courseModel); // Use the same Add view, but pass in the pre-selected program
-        }
-
-
-        // POST: Adds a new course to the database
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Add([Bind("Id,Code,Name,Hours,IdProfessor,IdProgram")] CourseModel course)
-        {
-            if (HttpContext.Session.GetString("auth") != "true")
-            {
-                return RedirectToAction("Index", "Login");
-            }
-
-            if (ModelState.IsValid)
-            {
-                _context.Add(course);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            PopulateProfessorsDropDownList(course.IdProfessor);
-            PopulateProgramsDropDownList(course.IdProgram);
-            return View(course);
-        }
-
-        // Shows the form to edit an existing course
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (HttpContext.Session.GetString("auth") != "true")
-            {
-                return RedirectToAction("Index", "Login");
-            }
-
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var course = await _context.Courses.FindAsync(id);
-            if (course == null)
-            {
-                return NotFound();
-            }
-            PopulateProfessorsDropDownList(course.IdProfessor);
-            PopulateProgramsDropDownList(course.IdProgram);
-            return View(course);
-        }
-
-        // POST: Updates an existing course in the database
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Code,Name,Hours,IdProfessor,IdProgram")] CourseModel course)
-        {
-            if (HttpContext.Session.GetString("auth") != "true")
-            {
-                return RedirectToAction("Index", "Login");
-            }
-
-            if (id != course.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(course);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!CourseExists(course.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            PopulateProfessorsDropDownList(course.IdProfessor);
-            PopulateProgramsDropDownList(course.IdProgram);
-            return View(course);
-        }
-
-        // Shows a confirmation page for course deletion
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (HttpContext.Session.GetString("auth") != "true")
-            {
-                return RedirectToAction("Index", "Login");
-            }
-
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var course = await _context.Courses
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (course == null)
-            {
-                return NotFound();
-            }
-
-            return View(course);
-        }
-
-        // POST: Deletes a course from the database
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            if (HttpContext.Session.GetString("auth") != "true")
-            {
-                return RedirectToAction("Index", "Login");
-            }
-
-            var course = await _context.Courses.FindAsync(id);
-            _context.Courses.Remove(course);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-
-        private bool CourseExists(int id)
-        {
-            return _context.Courses.Any(e => e.Id == id);
-        }
-
-        private void PopulateProfessorsDropDownList(object selectedProfessor = null)
-        {
-            var professorsQuery = from d in _context.Users
-                                where d.SetAsAdmin == false
-                                orderby d.FirstName
-                                select new
-                                {
-                                    d.Id,
-                                    ProfessorName = d.FirstName + " " + d.LastName
-                                };
-            ViewBag.IdProfessor = new SelectList(professorsQuery.AsNoTracking(), "Id", "ProfessorName", selectedProfessor);
-        }
-
-        private void PopulateProgramsDropDownList(object selectedProgram = null)
-        {
-            int? programId = selectedProgram as int?;
-            var programsQuery = from p in _context.Programs
-                                where p.Id == programId
-                                select p;
-            ViewBag.IdProgram = new SelectList(programsQuery.AsNoTracking(), "Id", "Name", selectedProgram);
-        }
-    
-        public IActionResult AddSubmit(CourseModel course)
-        {
-            // if auth is not  = true, it re-directs to the login screen.
-            if (HttpContext.Session.GetString("auth") != "true")
-            {
-                return RedirectToAction("Index", "Login");
-            }
-
-            // add the course to the list of courses
-            _context.Courses.Add(course);
-            //save changes to the database
-            _context.SaveChanges();
-            return RedirectToAction("Index", "Admin");
-        }
-    
+      _context = context;
     }
-    
+
+    // Displays a list of all courses
+    public async Task<IActionResult> Index()
+    {
+      if (HttpContext.Session.GetString("auth") != "true")
+      {
+        return RedirectToAction("Index", "Login");
+      }
+
+      var courses = await _context.Courses.Include(c => c.Professor).Include(c => c.TechClasses).ToListAsync();
+      return View(courses);
+    }
+
+    // Displays details for a specific course
+    public async Task<IActionResult> Details(int? id)
+    {
+      if (HttpContext.Session.GetString("auth") != "true")
+      {
+        return RedirectToAction("Index", "Login");
+      }
+
+      if (id == null)
+      {
+        return NotFound();
+      }
+
+      var course = await _context.Courses
+          .Include(c => c.Professor)
+          .Include(c => c.TechClasses)
+          .FirstOrDefaultAsync(m => m.Id == id);
+
+      if (course == null)
+      {
+        return NotFound();
+      }
+
+      return View(course);
+    }
+
+    // // Shows the form to add a new course
+    // public IActionResult Add()
+    // {
+    //     if (HttpContext.Session.GetString("auth") != "true")
+    //     {
+    //         return RedirectToAction("Index", "Login");
+    //     }
+
+    //     PopulateProfessorsDropDownList();
+    //     PopulateProgramsDropDownList();
+    //     return View(new CourseModel()); // Passes an empty instance to the View
+    // }
+
+
+    // Shows the form to add a new course for a specific program
+    [Route("/Course/AddCourse/{programID:int}")]
+    public IActionResult AddCourse(int programId)
+    {
+      if (HttpContext.Session.GetString("auth") != "true")
+      {
+        return RedirectToAction("Index", "Login");
+      }
+
+      PopulateProfessorsDropDownList();
+      PopulateProgramsDropDownList(programId); // Pass the programId to pre-select the program in the dropdown
+
+      var courseModel = new CourseModel();
+
+      courseModel.IdProgram = programId; // Pre-select the program if an ID was provided
+
+
+      return View("~/Views/Course/AddCourse.cshtml", courseModel);
+    }
+
+
+    // POST: Adds a new course to the database
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Add([Bind("Id,Code,Name,Hours,IdProfessor,IdProgram")] CourseModel course)
+    {
+      if (HttpContext.Session.GetString("auth") != "true")
+      {
+        return RedirectToAction("Index", "Login");
+      }
+
+      if (ModelState.IsValid)
+      {
+        _context.Add(course);
+        await _context.SaveChangesAsync();
+        return RedirectToAction(nameof(Index));
+      }
+      PopulateProfessorsDropDownList(course.IdProfessor);
+      PopulateProgramsDropDownList(course.IdProgram);
+      return View(course);
+    }
+
+    // Shows the form to edit an existing course
+    public async Task<IActionResult> Edit(int? id)
+    {
+      if (HttpContext.Session.GetString("auth") != "true")
+      {
+        return RedirectToAction("Index", "Login");
+      }
+
+      if (id == null)
+      {
+        return NotFound();
+      }
+
+      var course = await _context.Courses.FindAsync(id);
+      if (course == null)
+      {
+        return NotFound();
+      }
+      PopulateProfessorsDropDownList(course.IdProfessor);
+      PopulateProgramsDropDownList(course.IdProgram);
+      return View(course);
+    }
+
+    // POST: Updates an existing course in the database
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Edit(int id, [Bind("Id,Code,Name,Hours,IdProfessor,IdProgram")] CourseModel course)
+    {
+      if (HttpContext.Session.GetString("auth") != "true")
+      {
+        return RedirectToAction("Index", "Login");
+      }
+
+      if (id != course.Id)
+      {
+        return NotFound();
+      }
+
+      if (ModelState.IsValid)
+      {
+        try
+        {
+          _context.Update(course);
+          await _context.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException)
+        {
+          if (!CourseExists(course.Id))
+          {
+            return NotFound();
+          }
+          else
+          {
+            throw;
+          }
+        }
+        return RedirectToAction(nameof(Index));
+      }
+      PopulateProfessorsDropDownList(course.IdProfessor);
+      PopulateProgramsDropDownList(course.IdProgram);
+      return View(course);
+    }
+
+    // Shows a confirmation page for course deletion
+    public async Task<IActionResult> Delete(int? id)
+    {
+      if (HttpContext.Session.GetString("auth") != "true")
+      {
+        return RedirectToAction("Index", "Login");
+      }
+
+      if (id == null)
+      {
+        return NotFound();
+      }
+
+      var course = await _context.Courses
+          .FirstOrDefaultAsync(m => m.Id == id);
+      if (course == null)
+      {
+        return NotFound();
+      }
+
+      return View(course);
+    }
+
+    // POST: Deletes a course from the database
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> DeleteConfirmed(int id)
+    {
+      if (HttpContext.Session.GetString("auth") != "true")
+      {
+        return RedirectToAction("Index", "Login");
+      }
+
+      var course = await _context.Courses.FindAsync(id);
+      _context.Courses.Remove(course);
+      await _context.SaveChangesAsync();
+      return RedirectToAction(nameof(Index));
+    }
+
+    private bool CourseExists(int id)
+    {
+      return _context.Courses.Any(e => e.Id == id);
+    }
+
+    private void PopulateProfessorsDropDownList(object selectedProfessor = null)
+    {
+      var professorsQuery = from d in _context.Users
+                            where d.SetAsAdmin == false
+                            orderby d.FirstName
+                            select new
+                            {
+                              d.Id,
+                              ProfessorName = d.FirstName + " " + d.LastName
+                            };
+      ViewBag.IdProfessor = new SelectList(professorsQuery.AsNoTracking(), "Id", "ProfessorName", selectedProfessor);
+    }
+
+    private void PopulateProgramsDropDownList(object selectedProgram = null)
+    {
+      int? programId = selectedProgram as int?;
+      var programsQuery = from p in _context.Programs
+                          where p.Id == programId
+                          select p;
+      ViewBag.IdProgram = new SelectList(programsQuery.AsNoTracking(), "Id", "Name", selectedProgram);
+    }
+
+    public IActionResult AddSubmit(CourseModel course)
+    {
+      // if auth is not  = true, it re-directs to the login screen.
+      if (HttpContext.Session.GetString("auth") != "true")
+      {
+        return RedirectToAction("Index", "Login");
+      }
+
+      // add the course to the list of courses
+      _context.Courses.Add(course);
+      //save changes to the database
+      _context.SaveChanges();
+      return RedirectToAction("Index", "Admin");
+    }
+
+  }
+
 }
--- Models/CourseModel.cs
+++ Models/CourseModel.cs
@@ -3,35 +3,37 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ClassScheduling_WebApp.Models
 {
+  [Table("TblCourse")]
+  public class CourseModel
+  {
+    [Key]
+    public int Id { get; set; }
 
-    [Table("TblCourse")]
-    public class CourseModel
-    {
-        [Key]
-        public int Id { get; set; }
+    [Required(ErrorMessage = "Course code is required.")]
+    [StringLength(10, ErrorMessage = "Course code cannot exceed 10 characters.")]
+    public string Code { get; set; }
 
-        [Required(ErrorMessage = "Course code is required.")]
-        [StringLength(10, ErrorMessage = "Course code cannot exceed 10 characters.")]
-        public string Code { get; set; }
+    [Required(ErrorMessage = "Course name is required.")]
+    [StringLength(50, ErrorMessage = "Course name cannot exceed 50 characters.")]
+    public string Name { get; set; }
 
-        [Required(ErrorMessage = "Course name is required.")]
-        [StringLength(50, ErrorMessage = "Course name cannot exceed 50 characters.")]
-        public string Name { get; set; }
+    [Required(ErrorMessage = "Hours are required.")]
+    public int Hours { get; set; }
 
-        [Required(ErrorMessage = "Hours are required.")]
-        public int Hours { get; set; }
+    [ForeignKey("IdProfessor"), Required(ErrorMessage = "Professor is required.")]
+    public int IdProfessor { get; set; }
+    public virtual UserModel Professor { get; set; }
 
-        [ForeignKey("IdProfessor"), Required(ErrorMessage = "Professor is required.")]
-        public int IdProfessor { get; set; }
-        public virtual UserModel Professor { get; set; }
+    [ForeignKey("IdProgram"), Required(ErrorMessage = "Program is required.")]
+    public int IdProgram { get; set; }
+    public virtual ProgramModel Programs { get; set; }
 
-        [ForeignKey("IdProgram"), Required(ErrorMessage = "Program is required.")]
-        public int IdProgram { get; set; }
-        public virtual ProgramModel Programs { get; set; }
+    // Collection navigation properties
+    public virtual ICollection<TechClassModel> TechClasses { get; set; } = new List<TechClassModel>();
+    public virtual ICollection<ScheduleModel> Schedules { get; set; } = new List<ScheduleModel>();
 
-        // Collection navigation properties
-        public virtual ICollection<TechClassModel> TechClasses { get; set; } = new List<TechClassModel>();
-        public virtual ICollection<ScheduleModel> Schedules { get; set; } = new List<ScheduleModel>();
-    }
-
+    // New property for Program Name and Year (nullable)
+    [NotMapped] // This property is not mapped to the database
+    public string? ProgramNameAndYear { get; set; }
+  }
 }
