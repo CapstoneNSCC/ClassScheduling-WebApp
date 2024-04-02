f53f3238f025f02fa42ab31c85c3dddf97e68f90
Kayla Gillam-Wright <kd.gillamwright@gmail.com>
2024-04-02 11:59:53 -0300

    completed classrooms views, redirects all working, functionaility checked and working

--- Models/ClassroomModel.cs
+++ Models/ClassroomModel.cs
@@ -4,21 +4,21 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace ClassScheduling_WebApp.Models
 {
 
-    [Table("TblClassroom")]
-    public class ClassroomModel
-    {
-        [Key]
-        public int Id { get; set; }
+  [Table("TblClassroom")]
+  public class ClassroomModel
+  {
+    [Key]
+    public int Id { get; set; }
 
-        [Required(ErrorMessage = "Room number is required.")]
-        public int RoomNumber { get; set; }
+    [Required(ErrorMessage = "Room number is required.")]
+    public int RoomNumber { get; set; }
 
-        [Required(ErrorMessage = "Building acronym is required.")]
-        [StringLength(5, ErrorMessage = "Building acronym cannot exceed 5 characters.")]
-        public string BuildingAcronym { get; set; }
+    [Required(ErrorMessage = "Building acronym is required.")]
+    [StringLength(5, ErrorMessage = "Building acronym cannot exceed 5 characters.")]
+    public string BuildingAcronym { get; set; }
 
-        [NotMapped]
-        public string CombinedRoom => $"{RoomNumber}{BuildingAcronym}";
-    }
-    
+    [NotMapped]
+    public string CombinedRoom => $"{RoomNumber}-{BuildingAcronym}";
+  }
+
 }
