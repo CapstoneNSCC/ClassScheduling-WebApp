434be801d8c04d8f6a360aabc79082337a62c375
Kayla Gillam-Wright <kd.gillamwright@gmail.com>
2024-04-04 00:02:51 -0300

    adding technology list to classroom add and edit views

--- Models/TechRoomModel.cs
+++ Models/TechRoomModel.cs
@@ -3,18 +3,20 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ClassScheduling_WebApp.Models
 {
-    [Table("TblTechRoom")]
-    public class TechRoomModel
-    {
-        [ForeignKey("Room")]
-        public int IdRoom { get; set; }
+  [Table("TblTechRoom")]
+  public class TechRoomModel
+  {
+    [ForeignKey("Room")]
+    public int IdRoom { get; set; }
 
-        [ForeignKey("Technology")]
-        public int IdTechnology { get; set; }
+    [ForeignKey("Technology")]
+    public int IdTechnology { get; set; }
 
-        // Navigation properties
-        public virtual ClassroomModel Room { get; set; }
-        public virtual TechnologyModel Technology { get; set; }
-    }
+    // Navigation properties
+    [Required(ErrorMessage = "Room is required.")]
+    public virtual ClassroomModel Room { get; set; }
+
+    [Required(ErrorMessage = "Technology is required.")]
+    public virtual TechnologyModel Technology { get; set; }
+  }
 }
-
