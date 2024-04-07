0beaf59c5725a6cac9f674140fa424bfa872ae70
Kayla Gillam-Wright <kd.gillamwright@gmail.com>
2024-04-07 17:59:16 -0300

    added  eventModel content that was missing

---
+++ Models/EventModel.cs
@@ -0,0 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ClassScheduling_WebApp.Models
+{
+  [Table("TblEvents")]
+  public class EventModel
+  {
+    public int Id { get; set; }
+
+    public string title { get; set; }
+
+    public string description { get; set; }
+
+    public string dayOfWeek { get; set; }
+
+    public DateTime StartTime { get; set; }
+    public DateTime EndTime { get; set; }
+    public string teacher { get; set; }
+    public string Classroom { get; set; }
+  }
+
+}
